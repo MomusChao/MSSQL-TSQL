372ff22634cbe3c2bb5f15d22d97871597d6c772
Chao Ching Tou <air_jordan981182@yahoo.com.tw>
2018-12-06 11:11:19 +0800

    Insert into

---
+++ SQL/06Insert_into.sql
@@ -0,0 +1,8 @@
+/* first way */
+INSERT INTO table_name (column1, column2, ...)
+VALUES (value1, value2,...);
+
+/* second way */
+/* adding values for all the columns of the table */
+INSERT INTO table_name
+VALUES (value1, value2,...);
