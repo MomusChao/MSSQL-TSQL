53026c3442d9adebd59cab9fc0be63e91391803c
Chao Ching Tou <air_jordan981182@yahoo.com.tw>
2018-12-06 11:43:31 +0800

    Test Null values

---
+++ SQL/07Test_Null_values.sql
@@ -0,0 +1,9 @@
+/* IS NULL Syntax */
+SELECT column_names
+FROM table_name
+WHERE column_name IS NULL;
+
+/* IS NOT NULL Syntax */
+SELECT column_names
+FROM table_name
+WHERE column_name IS NOT NULL;
