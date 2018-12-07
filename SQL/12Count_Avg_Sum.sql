18d14f034ab0a2994a63aff3aa9d22a73565f0d3
Chao Ching Tou <air_jordan981182@yahoo.com.tw>
2018-12-07 10:13:34 +0800

    Count & Avg & Sum

---
+++ SQL/12Count_Avg_Sum.sql
@@ -0,0 +1,14 @@
+/* COUNT() Syntax */
+SELECT COUNT(column_name)
+FROM table_name
+WHERE condition;
+
+/* AVG() Syntax */
+SELECT AVG(column_name)
+FROM table_name
+WHERE condition;
+
+/* SUM() Syntax */
+SELECT SUM(column_name)
+FROM table_name
+WHERE condition;
