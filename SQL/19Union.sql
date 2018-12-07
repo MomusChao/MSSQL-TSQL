159b02ae65859428798c96f1fd954260c0882add
Chao Ching Tou <air_jordan981182@yahoo.com.tw>
2018-12-07 10:15:36 +0800

    Union

---
+++ SQL/19Union.sql
@@ -0,0 +1,9 @@
+/* UNION Syntax */
+SELECT column_name FROM table1
+UNION
+SELECT column_name FROM table2;
+
+/* UNION ALL Syntax */
+SELECT column_name FROM table1
+UNION ALL
+SELECT column_name FROM table2;
