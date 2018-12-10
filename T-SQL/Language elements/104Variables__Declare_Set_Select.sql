7b77dba272060a115c55cdfa28715a9a5de9b068
Chao Ching Tou <air_jordan981182@yahoo.com.tw>
2018-12-10 10:40:15 +0800

    Variables: Declare Set Select

---
+++ T-SQL/Language elements/104Variables__Declare_Set_Select.sql
@@ -0,0 +1,9 @@
+/* Variables in SQL procedures are defined by using the DECLARE statement */
+DECLARE  @NewBalance  int ;  
+
+/* Values can be assigned to variables using the SET statement or the SELECT INTO statement or as a default value when the variable is declared */
+SET  @NewBalance  =  10; 
+
+/* Select */
+DECLARE @var1 varchar(30);         
+SELECT @var1 = 'Generic Name';    
