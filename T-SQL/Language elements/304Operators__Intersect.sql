384937e1b12149714314fd9a6fd491af30a10517
Chao Ching Tou <air_jordan981182@yahoo.com.tw>
2018-12-11 11:15:49 +0800

    Operators: Intersect

---
+++ T-SQL/Language elements/304Operators__Intersect.sql
@@ -0,0 +1,6 @@
+/* Return any distinct values that are returned by both the query on the left and right sides of the INTERSECT operator */
+SELECT column1   
+FROM table_name1 
+INTERSECT  
+SELECT column1   
+FROM table_name2; 
